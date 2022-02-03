c0baac5eb7caace9a5126735b8b1bf5433d0ca08
disaylt <disofban@gmail.com>
2022-02-03 20:56:09 +0300

    update

---
+++ TemplateTelegramBot/Log/GeneralExceptionsPusher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateTelegramBot.Log
+{
+    internal static class GeneralExceptionsPusher
+    {
+        internal static IExeptionLogger? ExceptionPusher { get; set; }
+    }
+}
